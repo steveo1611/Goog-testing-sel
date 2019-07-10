9b241e8f9975fd37fb46731d84287fab8a463f81
Steve Oldfield <steveold1611@gmail.com>
2019-07-09 21:36:51 -0600

    Add project files.

---
+++ Goog-testing-sel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Goog_testing_sel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
